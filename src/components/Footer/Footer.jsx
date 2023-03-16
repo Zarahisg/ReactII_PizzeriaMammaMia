aafbf55ce3602eef12c434855b4515cd0a36ceef
Zara_G <consultanutriguzman@gmail.com>
2023-03-16 18:35:16 -0300

    Commit Global

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import { FaInstagram, FaTwitter, FaFacebookF, FaTiktok } from "react-icons/fa";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>© Desafio Latam 2023 React II- Pizzería Mamma Mía</p>
+    </footer>
+  );
+};
+
+export default Footer;
